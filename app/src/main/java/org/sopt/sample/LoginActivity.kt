1ce937581cc32f382449743ccae61a1f8ef7195c
waterminn <tnals08019@gmail.com>
2022-10-09 10:52:10 +0900

    [refactor] 함수 위치, 오타, 불필요한 공백 수정

--- app/src/main/java/org/sopt/sample/LoginActivity.kt
+++ app/src/main/java/org/sopt/sample/LoginActivity.kt
@@ -10,23 +10,37 @@ import com.google.android.material.snackbar.Snackbar
 import org.sopt.sample.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var binding: ActivityLoginBinding
     private var userInfo: User? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnToSignup.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            resultLauncher.launch(intent)
+        }
+        binding.btnLoginSubmit.setOnClickListener {
+            executeLogin()
+        }
+    }
 
     // 회원가입 페이지에서 입력한 정보 가져오기
     private val resultLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
-    ) {result ->
+    ) { result ->
         if (result.resultCode == Activity.RESULT_OK) {
             Snackbar.make(binding.root, R.string.sign_up_success, Snackbar.LENGTH_SHORT).show()
-            val info : Intent? = result.data
+            val info: Intent? = result.data
             userInfo = info?.getSerializableExtra("info") as User
         }
     }
 
     // 로그인 성공 여부 판단
-    private fun isMember(inputId : String, inputPw : String, id : String?, pw : String?) : Boolean
-    = inputId == id && inputPw == pw
+    private fun isMember(inputId: String, inputPw: String, id: String?, pw: String?): Boolean =
+        inputId == id && inputPw == pw
 
     // 홈 페이지로 이동
     private fun intentToHome() {
@@ -44,7 +58,7 @@ class LoginActivity : AppCompatActivity() {
         val inputId = binding.etLoginId.text.toString()
         val inputPw = binding.etLoginPw.text.toString()
 
-        if (inputId.isEmpty() && inputPw.isEmpty()){
+        if (inputId.isEmpty() && inputPw.isEmpty()) {
             return Snackbar.make(binding.root, R.string.login_empty, Snackbar.LENGTH_SHORT).show()
         }
         if (!isMember(inputId, inputPw, userInfo?.id, userInfo?.pw)) {
@@ -52,19 +66,5 @@ class LoginActivity : AppCompatActivity() {
         }
         Toast.makeText(this, R.string.login_success, Toast.LENGTH_SHORT).show()
         intentToHome()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnToSignup.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            resultLauncher.launch(intent)
-        }
-        binding.btnLoginSubmit.setOnClickListener {
-            executeLogin()
-        }
     }
 }
