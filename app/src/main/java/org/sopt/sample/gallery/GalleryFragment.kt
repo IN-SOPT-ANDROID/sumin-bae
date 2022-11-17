fb942fc969c4ead1b87c558748f9c6363d73ee92
waterminn <tnals08019@gmail.com>
2022-11-17 11:46:36 +0900

    [feat] 갤러리 pager 컴포넌트 추가

--- app/src/main/java/org/sopt/sample/gallery/GalleryFragment.kt
+++ app/src/main/java/org/sopt/sample/gallery/GalleryFragment.kt
@@ -6,20 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import org.sopt.sample.R
 import org.sopt.sample.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
     private var _binding: FragmentGalleryBinding? = null
-    private val binding: FragmentGalleryBinding
-        get() = requireNotNull(_binding) { "value of _binding is null" }
+    private val binding get() = requireNotNull(_binding) { "value of _binding is null" }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
+    ): View {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vpGallery.adapter = GalleryAdapter().apply {
+            setItems(
+                listOf(
+                    R.drawable.ic_launcher_background,
+                    R.drawable.ic_launcher_background
+                )
+            )
+        }
     }
 
     override fun onDestroyView() {
